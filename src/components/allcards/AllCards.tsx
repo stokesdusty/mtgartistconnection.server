95df4ebe16bf274ce9433b4943328a26e70fc802
Dustin Stokes <78513596+stokesdusty@users.noreply.github.com>
2025-10-25 23:30:55 -0700

    Update AllCards.tsx

--- src/components/allcards/AllCards.tsx
+++ src/components/allcards/AllCards.tsx
@@ -114,7 +114,8 @@ const AllCards = () => {
         return str
           .toLowerCase()
           .normalize("NFD") // Decompose combined characters
-          .replace(/[\u0300-\u036f]/g, ""); // Remove diacritics
+          .replace(/[\u0300-\u036f]/g, "") // Remove diacritics
+          .replace(/\./g, ""); // Remove periods
       };
 
       // Filter for artist match (exact or as part of multiple artists)
