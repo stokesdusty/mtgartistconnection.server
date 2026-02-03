0502c319a34b6c2a1e9e5d048399ba5101628c61
Dustin Stokes <78513596+stokesdusty@users.noreply.github.com>
2026-02-02 23:30:29 -0800

    fix for clickable event names

--- src/components/graphql/queries.ts
+++ src/components/graphql/queries.ts
@@ -47,6 +47,7 @@ export const GET_SIGNINGEVENTS = gql`
             city
             startDate
             endDate
+            url
         }
     }
 `;
@@ -67,6 +68,7 @@ export const GET_EVENTS_BY_ARTIST = gql`
             city
             startDate
             endDate
+            url
         }
     }
 `;
